3869ffd5c211c6c171dfe248979fab5118f79559
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-03-30 14:52:38 +0000

    adding static HTML structue of the exersice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import "./styles.css";
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <TipCalculator />
+    </div>
+  );
 }
 
-export default App
+function TipCalculator() {
+  return (
+    <div>
+      <BillInput />
+      <SelectPercentage>How did you like the service?</SelectPercentage>
+      <SelectPercentage>How did your friend like the service?</SelectPercentage>
+      <Output />
+      <Reset />
+    </div>
+  );
+}
+
+function BillInput() {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input type="text" placeholder="Bill value" />
+    </div>
+  );
+}
+
+function SelectPercentage({ children }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">It was okay (5%)</option>
+        <option value="10">It was good (10%)</option>
+        <option value="20">Absolutely amazing! (20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function Output() {
+  return <h3>You pay $0 ($0 + $0 tip)</h3>;
+}
+
+function Reset() {
+  return <button>Reset</button>;
+}
